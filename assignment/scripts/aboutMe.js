3abf5446659bf2d257fb355dd01ce6bab73eaa71
Alvin Graham <grah0071@gmail.com>
2023-11-05 22:07:47 -0600

    Completed stretch steps

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -70,7 +70,7 @@ console.log(result);
 //      Console log the value of `diceRoll`
 let diceRoll = 'Try again later.';
 if (luckyNumber === 2 && adventurous){
-  diceRoll = 'Roll the dice';
+  diceRoll = 'Roll the dice!';
 }
 console.log(diceRoll);
 
@@ -106,6 +106,23 @@ console.log(petStatus);
 //      assigns the highest value to `mostPets`. There's several possibilities --
 //      be sure to think through all the scenarios. 
 //      console.log `mostPets` after the conditional has run.
+let mostPets;
+if (pets === 0 && friendsPets === 0){
+  mostPets = 0;
+  console.log('Neither of us have any pets');
+} else if (pets === friendsPets){
+  mostPets = pets;
+  console.log('We have the same number of pets');
+} else if (pets > friendsPets){
+  mostPets = pets;
+  console.log('I have more pets');
+} else {
+  mostPets = friendsPets;
+  console.log('My friend has more pets');
+}
+console.log(mostPets);
+
+
 
 // 16 - Make a variable called `luckyResult`
 //      Write a *switch* statement that sets `luckyResult` to:
@@ -115,9 +132,34 @@ console.log(petStatus);
 //      Otherwise, "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 //      console.log `luckyResult` after the conditional has run.
+let luckyResult;
+
+switch (luckyNumber) {
+  case 1:
+    luckyResult = 'First is the worst';
+    break;
+  case 2:
+    luckyResult = 'Second is the best';
+    break;
+  case 3:
+    luckyResult = 'Third is the one with the polka dot dress';
+    break;
+  default:
+    luckyResult = 'Luck is what happens when preparation meets opportunity';
+}
+console.log(luckyResult);
 
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
 
+// 12 (STRETCH) - Reassign result variable to undefined. Create a "ternary if", where
+//      if adventurous is true, set `result` to be "Adventures are great!", 
+//      if it's not true,  set `result` to be "How about we stay home?"
+//      Console log the value of `result`
+
+result = undefined; //reset result to null undefined value
+
+adventurous ? result = "Adventures are great!" :  result = "How about we stay home?";
+console.log(result);
 
 
 
